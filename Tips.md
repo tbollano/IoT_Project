7e904cfb1dd874ecc99bcd4a2d8fb5d916cc0e3c
tbollano <t.bollano@student.tue.nl>
2023-11-20 11:46:38 +0100

    just learning :)

---
+++ Tips.md
@@ -0,0 +1,3 @@
+1-Using the method get for html while your antivirus
+is running will lead to an error. This is your 
+antivirus protecting you :) dont punch your monitor.
--- app.py
+++ app.py
@@ -1,12 +1,54 @@
-from flask import Flask
+from flask import Flask,redirect,url_for,request
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/hello/<name>') ##modelled as an input from html
+def hello_world(name):  # put application's code here
+    return 'Hello %s :)))!' % name  ##val assigned just like C
+app.add_url_rule('/','hello',hello_world)
+    ##binds this function to the URL 'hello'
+    #which is http://localhost:5000/hello
+@app.route('/admin')
+def sup_admin():
+    return 'Hello Admin'
+@app.route('/guest/<guest>')
+def sup_guest(guest):
+    if guest =='Admin':
+        return redirect(url_for('sup_admin'))
+    else:
+        return 'Hello %s' %guest
+@app.route('/success/<name>')
+def success(name):
+    return 'welcome %s' % name
+@app.route('/login',methods =['POST','GET'])
+def login():
+    if request.method =='POST':
+        user=request.form['nm']
+        return redirect(url_for('success',name=user))
+    else:
+        user = request.args.get('nm')
+        return redirect(url_for('success',name=user))
+@app.route('/user/<name>')
+def sup_user(name):
+    if name == 'admin':
+        return redirect(url_for('sup_admin'))
+    else:
+        return redirect(url_for('sup_guest', guest=name))
+@app.route('/canonical/<float:attitude>')
+def get_attitude(attitude):
 
-
+    if attitude <0:
+        return 'Heading for the ground, your attitude is %f' %attitude
+    elif attitude >0:
+        return 'You are not flying a rocked, your attitude is %f!!' %attitude
+    else:
+        return 'Smooth sailing at %f attitude' %attitude
+##this is taken into account here but /canonical/ allows for both / canonical
+# and /canonical/ in the URL
 if __name__ == '__main__':
+    app.debug = True
     app.run()
+    app.run(debug = True)
+
+
